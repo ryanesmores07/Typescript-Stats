69ba713f0243cb070958e26851ad17ba86fadeef
Ryan Esmores <ryanesmores07@gmail.com>
2023-03-15 23:03:48 +0900

    finished stats project

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
-import { CsvFileReader } from "./CsvFileReader";
+import { Summary } from "./Summary";
 
-const csvFileReader = new CsvFileReader("football.csv");
-
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv("football.csv");
 matchReader.load();
 
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-  if (
-    (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
-    (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
-  ) {
-    manUnitedWins++;
-  }
-}
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
-console.log(manUnitedWins);
+summary.buildAndPrintReport(matchReader.matches);
